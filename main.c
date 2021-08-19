beac74a30826f9464076465c3a0a5b80a5f7079f
CHARAN <63697628+charanquartz@users.noreply.github.com>
2021-08-20 01:04:22 +0530

    z 2

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main() {
     int x,y;
-    x=1;
+    x=7;
     int result1 = 5 + ++x;
     printf("%d ",result1);
     y=1;
     int result2 = 5 + y++;
     printf("%d",result2);
 }
+123
